b9865b3fcf566bdecda091c6950c54433fb185eb
Matthias Totschnig <matthias@totschnig.org>
2017-12-28 01:13:25 +0100

    Improve testing and add new test function

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var diff = require('gulp-diff');
-var rename = require('gulp-rename');
 
 gulp.task('test', function () {
   return gulp.src('./test/**/*.scss')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(rename({extname: '.css'}))
-    .pipe(diff())
-    .pipe(diff.reporter({ fail: true }));
 });
 
 gulp.task('default', function () {
-  return gulp.watch('./test/**/*.scss', ['test']);
+  return gulp.watch(['./sass/**/*.scss', './test/**/*.scss'], ['test']);
 });
