4f377b8dff87ba2fd557b9696da0bdc9c3d236a4
Lu Nelson <lunelson@gmail.com>
2021-12-23 15:02:16 +0100

    preparatory housekeeping

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,14 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
